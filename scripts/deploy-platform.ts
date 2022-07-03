f84e61d9da8942fa9d289c79e60d44f1cd616966
roma <roma.klabukov@gmail.com>
2022-07-03 18:56:35 +0300

    refactooring

--- scripts/deploy-platform.ts
+++ scripts/deploy-platform.ts
@@ -10,7 +10,7 @@ async function main() {
         process.env.ACDM_ADDRESS || "0x42182E993d13181735190F2C3eAaA31c3200e749",
         process.env.XXX_ADDRESS || "0xB9c541db3F8f59b442D43e8EEa70043CC0e36b40",
         process.env.PLATFORM_ROUND_TIME || 3 * 24 * 60 * 60,
-      process.env.VOTING_ADDRESS || "0x582BDDF6edE0717f5B7403752fCFdF9D5f98a53e"
+        process.env.VOTING_ADDRESS || "0x582BDDF6edE0717f5B7403752fCFdF9D5f98a53e"
     );
 
     await platform.deployed();
--- scripts/deploy-staking.ts
+++ scripts/deploy-staking.ts
@@ -9,9 +9,9 @@ async function main() {
 
     const stakingFactory = await ethers.getContractFactory("Staking", owner);
     const staking = await stakingFactory.deploy(
-      process.env.LP_TOKEN_ADDRESS || "0x9aee43130694501e5e109644ddb176e35075928f", 
-      process.env.REWARD_TOKEN_ADDRESS || "0x42182E993d13181735190F2C3eAaA31c3200e749",
-      process.env.VOTING_ADDRESS || "0x582BDDF6edE0717f5B7403752fCFdF9D5f98a53e"
+        process.env.LP_TOKEN_ADDRESS || "0x9aee43130694501e5e109644ddb176e35075928f", 
+        process.env.REWARD_TOKEN_ADDRESS || "0x42182E993d13181735190F2C3eAaA31c3200e749",
+        process.env.VOTING_ADDRESS || "0x582BDDF6edE0717f5B7403752fCFdF9D5f98a53e"
     ); 
     await staking.deployed();
 
